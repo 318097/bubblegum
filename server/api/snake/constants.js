c31844aa1982f1a4376dfb964331f9d9fe1e3faa
Mehul <318097@gmail.com>
2019-05-04 16:53:15 +0530

    feat: snake game multiplayer

---
+++ server/api/snake/constants.js
@@ -0,0 +1,10 @@
+module.exports = {
+  NOT_STARTED: 'not-started',
+  FINISHED: 'finished',
+  PAUSED: 'paused',
+  INITIAL: 'none',
+  IN_PROGRESS: 'in-progress',
+  DEAD: 'dead',
+  ENDED: 'ended',
+  DISCONNECTED: 'disconnected',
+}; 
