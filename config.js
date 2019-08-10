66c759772e7ae0e03ae04b540d5ccac1a135a69a
Mehul <318097@gmail.com>
2019-08-10 14:29:41 +0530

    Fix: JWT in config

--- config.js
+++ config.js
@@ -2,9 +2,7 @@ const config = {
   PORT: process.env.PORT || 7000,
   DB_URL: process.env.DB_URL || "mongodb://localhost/bubblegum",
   expireTime: 24 * 60 * 10,
-  secrets: {
-    JWT: process.env.JWT || "brainbox"
-  }
+  JWT: process.env.JWT || "brainbox"
 };
 
 module.exports = config;
