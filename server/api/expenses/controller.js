597a486ba262f6f272ba9b51bb82fe51c52dafbb
Mehul <318097@gmail.com>
2019-09-16 20:29:16 +0530

    Fix: Project message for expense

--- server/api/expenses/controller.js
+++ server/api/expenses/controller.js
@@ -1,26 +1,31 @@
-const mongoose = require('mongoose');
-const moment = require('moment');
+const mongoose = require("mongoose");
+const moment = require("moment");
 
-const Model = require('./model');
-const UserModel = require('../user/model');
+const Model = require("./model");
+const UserModel = require("../user/model");
 
 const { ObjectId } = mongoose.Types;
 
-const updateCount = async ({ userId, expenseTypeId, value }) => await UserModel.findOneAndUpdate(
-  {
-    _id: userId,
-    'expenseTypes._id': ObjectId(expenseTypeId)
-  }, {
-    $inc: {
-      'expenseTypes.$.count': value
+const updateCount = async ({ userId, expenseTypeId, value }) =>
+  await UserModel.findOneAndUpdate(
+    {
+      _id: userId,
+      "expenseTypes._id": ObjectId(expenseTypeId)
+    },
+    {
+      $inc: {
+        "expenseTypes.$.count": value
+      }
     }
-  }
-);
+  );
 
-exports.getAllExpenseTypes = async (req, res, next) => res.send({ expenseTypes: req.user.expenseTypes })
+exports.getAllExpenseTypes = async (req, res, next) =>
+  res.send({ expenseTypes: req.user.expenseTypes });
 
 exports.getAllExpenses = async (req, res, next) => {
-  const result = await Model.find({ userId: req.user._id }, null, { sort: { createdAt: -1 } });
+  const result = await Model.find({ userId: req.user._id }, null, {
+    sort: { createdAt: -1 }
+  });
   res.send({ expenses: result });
 };
 
@@ -28,38 +33,43 @@ exports.getMonthlyExpense = async (req, res, next) => {
   const { month } = req.params;
   const { year = moment().year() } = req.query;
 
-  const monthStart = moment(`${month}-${year}`, 'MM-YYYY').startOf('month').toDate();
-  const monthEnd = moment(`${month}-${year}`, 'MM-YYYY').endOf('month').toDate();
+  const monthStart = moment(`${month}-${year}`, "MM-YYYY")
+    .startOf("month")
+    .toDate();
+  const monthEnd = moment(`${month}-${year}`, "MM-YYYY")
+    .endOf("month")
+    .toDate();
 
   const result = await UserModel.aggregate([
     {
       $match: {
-        _id: ObjectId(req.user._id),
+        _id: ObjectId(req.user._id)
       }
     },
     {
-      $unwind: { path: '$expenseTypes', preserveNullAndEmptyArrays: true }
+      $unwind: { path: "$expenseTypes", preserveNullAndEmptyArrays: true }
     },
     {
       $lookup: {
-        from: 'expenses',
-        localField: 'expenseTypes._id',
-        foreignField: 'expenseTypeId',
-        as: 'expense'
+        from: "expenses",
+        localField: "expenseTypes._id",
+        foreignField: "expenseTypeId",
+        as: "expense"
       }
     },
     {
-      $unwind: { path: '$expense', preserveNullAndEmptyArrays: true }
+      $unwind: { path: "$expense", preserveNullAndEmptyArrays: true }
     },
     {
       $project: {
-        _id: '$expense._id',
-        expenseTypeId: '$expense.expenseTypeId',
-        amount: '$expense.amount',
-        userId: '$expense.userId',
-        createdAt: '$expense.createdAt',
-        expenseType: '$expenseTypes.name',
-        expenseGroup: '$expense.expenseGroup',
+        _id: "$expense._id",
+        expenseTypeId: "$expense.expenseTypeId",
+        amount: "$expense.amount",
+        userId: "$expense.userId",
+        createdAt: "$expense.createdAt",
+        expenseType: "$expenseTypes.name",
+        expenseGroup: "$expense.expenseGroup",
+        message: "$expense.message"
       }
     },
     {
@@ -83,7 +93,8 @@ exports.createExpenseType = async (req, res, next) => {
   const result = await UserModel.findOneAndUpdate(
     {
       _id: req.user._id
-    }, {
+    },
+    {
       $push: {
         expenseTypes: {
           name,
@@ -99,7 +110,11 @@ exports.createExpenseType = async (req, res, next) => {
 exports.createExpense = async (req, res, next) => {
   const { expenseTypeId, amount, message, expenseGroup } = req.body;
   const result = await Model.create({
-    expenseTypeId, amount, message, expenseGroup, userId: req.user._id
+    expenseTypeId,
+    amount,
+    message,
+    expenseGroup,
+    userId: req.user._id
   });
   updateCount({ userId: req.user._id, expenseTypeId, value: 1 });
   res.send({ result });
@@ -111,10 +126,11 @@ exports.updateExpenseType = async (req, res, next) => {
   const result = await UserModel.findOneAndUpdate(
     {
       _id: req.user._id,
-      'expenseTypes._id': expenseTypeId
-    }, {
+      "expenseTypes._id": expenseTypeId
+    },
+    {
       $set: {
-        'expenseTypes.$.name': name
+        "expenseTypes.$.name": name
       }
     }
   );
@@ -127,9 +143,13 @@ exports.updateExpense = async (req, res, next) => {
   const result = await Model.findOneAndUpdate(
     {
       _id: expenseId
-    }, {
+    },
+    {
       $set: {
-        expenseTypeId, amount, message, userId: req.user._id
+        expenseTypeId,
+        amount,
+        message,
+        userId: req.user._id
       }
     }
   );
@@ -141,7 +161,8 @@ exports.deleteExpenseType = async (req, res, next) => {
   const result = await UserModel.findOneAndUpdate(
     {
       _id: req.user._id
-    }, {
+    },
+    {
       $pull: {
         expenseTypes: {
           _id: ObjectId(expenseTypeId)
@@ -154,9 +175,11 @@ exports.deleteExpenseType = async (req, res, next) => {
 
 exports.deleteExpense = async (req, res, next) => {
   const expenseId = req.params.id;
-  const result = await Model.findOneAndDelete(
-    { _id: ObjectId(expenseId) }
-  );
-  updateCount({ userId: req.user._id, expenseTypeId: result.expenseTypeId, value: -1 });
+  const result = await Model.findOneAndDelete({ _id: ObjectId(expenseId) });
+  updateCount({
+    userId: req.user._id,
+    expenseTypeId: result.expenseTypeId,
+    value: -1
+  });
   res.send({ result });
 };
