d3821f2a67626ad9b3169f6778a9dacfba97d9c0
Mehul <318097@gmail.com>
2019-09-17 09:27:51 +0530

    Fix: Connect to correct db

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 
+const env = process.env.NODE_ENV;
+
 const config = {
   PORT: process.env.PORT || 7000,
-  DB_URL: process.env.DB_URL || "mongodb://localhost/bubblegum",
+  DB_URL:
+    env === "production" ? process.env.DB_URL : "mongodb://localhost/bubblegum",
   JWT: process.env.JWT || "brainbox",
   GOOGLE_LOGIN_CLIENT_ID: process.env.GOOGLE_LOGIN_CLIENT_ID
 };
