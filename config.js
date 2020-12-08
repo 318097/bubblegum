849cdf3b9b853001ce2d01c7eab8db29b3357dbf
Mehul Lakhanpal <318097@gmail.com>
2020-12-08 17:09:50 +0530

    Add: Staging URL

--- config.js
+++ config.js
@@ -5,9 +5,13 @@ const env = process.env.NODE_ENV;
 const config = {
   PORT: process.env.PORT || 7000,
   DB_URL:
-    env === "production" ? process.env.DB_URL : "mongodb://localhost/bubblegum",
+    env === "production"
+      ? process.env.DB_URL
+      : env === "staging"
+      ? process.env.STAGING_DB_URL
+      : "mongodb://localhost/bubblegum",
   JWT: process.env.JWT || "brainbox",
-  GOOGLE_LOGIN_CLIENT_ID: process.env.GOOGLE_LOGIN_CLIENT_ID
+  GOOGLE_LOGIN_CLIENT_ID: process.env.GOOGLE_LOGIN_CLIENT_ID,
 };
 
 module.exports = config;
