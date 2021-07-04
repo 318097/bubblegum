e885e10119ea1e6029c370ee1948a0cf4a0798b6
Mehul Lakhanpal <318097@gmail.com>
2021-07-04 17:32:01 +0530

    Update: Expense base colors

--- server/constants.js
+++ server/constants.js
@@ -12,11 +12,11 @@ const APP_INFO = {
 const APP_LIST = Object.keys(APP_INFO);
 
 const defaultExpenseTypes = [
-  { key: "EXPENSE", label: "Expense", success: "DOWN" },
-  { key: "BILLS", label: "Bills", success: "DOWN" },
+  { key: "EXPENSE", label: "Expense", success: "DOWN", color: "watermelon" },
+  { key: "BILLS", label: "Bills", success: "DOWN", color: "orange" },
   // { key: "LOAN", label: "Lend" },
-  { key: "INVESTMENT", label: "Investment", success: "UP" },
-  { key: "INCOME", label: "Income", success: "DOWN" },
+  { key: "INVESTMENT", label: "Investment", success: "UP", color: "green" },
+  { key: "INCOME", label: "Income", success: "DOWN", color: "orchid" },
 ];
 
 module.exports = {
