645409d27692356633171952b7c98b11c2dfce42
Mehul Lakhanpal <318097@gmail.com>
2026-04-12 14:40:34 +0530

    LAST LEGACY  COMMIT

--- server/middleware/app-middleware.js
+++ server/middleware/app-middleware.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-const override = require("method-override");
 const _ = require("lodash");
 const { PRODUCT_LIST } = require("../utils/products");
 const { getToken } = require("../utils/authentication");
@@ -26,6 +25,5 @@ module.exports = function (app) {
   app.use(express.json({ limit: "1mb" })); // for parsing application/json
   app.use(express.urlencoded({ extended: true }));
   app.use(cors());
-  app.use(override());
   app.use(appendSourceInfo);
 };
