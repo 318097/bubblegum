957ab52db832aef687d8bf60ea855326f9941f48
Mehul Lakhanpal <318097@gmail.com>
2026-04-28 09:16:17 +0530

    Fix: Correct controller method name for liquid-tech route

--- server/api/notion/notion.routes.js
+++ server/api/notion/notion.routes.js
@@ -5,7 +5,7 @@ import * as controller from "./notion.controller.js";
 const router = Router();
 
 router.get("/keybindings", transparent, controller.getAllKeyBindings);
-router.get("/liquid-tech", transparent, controller.getLiquidTechniques);
+router.get("/liquid-tech", transparent, controller.getLiquidTech);
 router.get("/vocab", transparent, controller.getVocab);
 
 export default router;
